71bfeff1b52e1c34790f6e7319c4b1ed59207439
Łukasz Łosieczka <lukasz.losieczka@gmail.com>
2023-10-16 13:18:43 +0200

    updated db connection

--- dbConnection.php
+++ dbConnection.php
@@ -1,8 +1,8 @@
 <?php
 $databaseHost = 'localhost';
-$databaseName = 'test';
-$databaseUsername = 'root';
-$databasePassword = 'root';
+$databaseName = 'phpcrudapp';
+$databaseUsername = 'user1';
+$databasePassword = 'user1';
 
 // Open a new connection to the MySQL server
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
